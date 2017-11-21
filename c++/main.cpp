91759c109549fb356e04fbafe7b620e715dab79c
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2017-11-21 20:33:17 +0700

    sample c++ code

---
+++ c++/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void input(int&, int&, bool&);
+void multiply(int&, int&, int&);
+void divide(int&, int&, short&, short&);
+void output(int&, short&, short&, bool&);
+
+int main(int argc, char** argv) {
+      int lhs        = 0;
+      int rhs        = 0;
+      int tich       = 0;
+      short quotient = 0;
+      short ramainer = 0;
+      bool hexflag   = 0;
+
+      input(lhs, rhs, hexflag);
+
+      multiply(lhs, rhs, tich);
+
+      divide(lhs, rhs, quotient, remainer);
+
+      output(tich, quotient, remainer hexflag);
+
+      return 0;
+}
---
+++ c++/output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void output(int& tich, short& quotient, short& remainer, bool& hexflag) {
+      if (!hexflag) {
+            std::cout << "Tich hai so la  : " << tich << std::endl;
+            std::cout << "Thuong hai so la: " << quotient << std::endl;
+            std::cout << "Du sau khi chia : " << remainer << std::endl;
+      }
+      else {
+            std::cout << "Tich hai so la  : " << std::hex << tich << std::endl;
+            std::cout << "Thuong hai so la: " << std::hex << quotient << std::endl;
+            std::cout << "Du sau khi chia : " << std::hex << remainer << std::endl;
+      }
+}
